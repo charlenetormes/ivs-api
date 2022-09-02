16b9097d5efc3eee7cd8b60bb71bb1fd5af7b146
charlenetormes <charlene.tormes@gmail.com>
2022-09-03 04:46:40 +0800

    Added cors middleware

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const userRoutes = require('./routes/users.routes')
 const auth = require('./middleware/auth');
 const app = express()
+const cors = require(cors())
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 app.use(auth)
 app.use(userRoutes)
