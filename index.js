8180adefca6fb60d4507b62036ee647f0e64b52f
charlenetormes <charlene.tormes@gmail.com>
2022-09-03 18:29:24 +0800

    Added server urls to the swagger definition

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const options = {
         },
         servers: [
             {
+                url: "http://ec2-54-157-14-17.compute-1.amazonaws.com:3000"
+            },
+            {
                 url: "http://localhost:3000"
             }
         ],
